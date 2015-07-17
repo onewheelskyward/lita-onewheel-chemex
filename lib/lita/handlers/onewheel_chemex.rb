a22ab753c05f03b79cfae9bd3acb963af8851839
Andrew Kreps <andrew.kreps@gmail.com>
2015-07-17 08:45:08 -0700

    Tests!

--- lib/lita/handlers/onewheel_chemex.rb
+++ lib/lita/handlers/onewheel_chemex.rb
@@ -1,8 +1,8 @@
 module Lita
   module Handlers
     class OnewheelChemex < Handler
-      route /chemex fresh/i, :chemex_fresh, command: true, help: 'Report a fresh chemex pour.'
-      route /chemex/i, :chemex_report, command: true, help: 'See when the last chemex was poured.'
+      route /^chemex fresh$/i, :chemex_fresh, command: true, help: 'Report a fresh chemex pour.'
+      route /^chemex$/i, :chemex_report, command: true, help: 'See when the last chemex was poured.'
 
       def chemex_fresh(response)
         redis.set('chemex', Time.now)
@@ -10,8 +10,14 @@ module Lita
       end
 
       def chemex_report(response)
-        chemex_age = Time::parse redis.get('chemex')
-        response.reply chemex_age.strftime('%H:%M:%S')
+        chemex_date = redis.get('chemex')
+        if chemex_date.nil?
+          response.reply 'There is no chemex.'
+          return
+        end
+
+        chemex_age = Time::parse chemex_date
+        response.reply "The chemex was brewed at #{chemex_age.strftime('%H:%M:%S')}"
       end
     end
 
--- spec/lita/handlers/onewheel_chemex_spec.rb
+++ spec/lita/handlers/onewheel_chemex_spec.rb
@@ -5,8 +5,8 @@ describe Lita::Handlers::OnewheelChemex, lita_handler: true do
     send_command 'chemex fresh'
     expect(replies.last).to eq('Fresh chemex is available now!')
   end
-  # it 'will track the age of a chemex pour' do
-  #   send_command 'chemex'
-  #   expect(replies.last).to eq('The chemex is x seconds old.')
-  # end
+  it 'will track the age of a chemex pour' do
+    send_command 'chemex'
+    expect(replies.last).to include('The chemex was brewed at')
+  end
 end
